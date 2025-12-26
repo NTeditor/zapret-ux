616e05145a175c5adea5638d5cf20c9b8decda1b
NTeditor <nteditor4@gmail.com>
2025-12-27 00:16:57 +0300

    add cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use camino::Utf8PathBuf;
+use clap::{Parser, Subcommand};
+
+#[cfg(target_os = "android")]
+const DEFAULT_CONFIG_PATH: &str = "/data/adb/zapret-ux/config.toml";
+
+#[cfg(not(target_os = "android"))]
+const DEFAULT_CONFIG_PATH: &str = "config.toml";
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, value_name = "FILE", default_value = DEFAULT_CONFIG_PATH)]
+    config: Utf8PathBuf,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Start nfqws daemon
+    Start,
+    /// Stop nfqws daemon
+    Stop,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    Ok(())
 }
